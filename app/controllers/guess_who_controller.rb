5cc12c7f1d46c233ce424ec28b533cfd997f2aab
Ajay Vallabh <ajay.vallabh@mail.com>
2014-01-10 11:23:08 -0500

    Added github auth, index page

---
+++ app/controllers/guess_who_controller.rb
@@ -0,0 +1,4 @@
+class GuessWhoController < ApplicationController
+  def index
+  end
+end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,9 @@
 class SessionsController < ApplicationController
+
+  def index
+
+  end
+
   def create
     redirect_to root_path, notice: 'You have successfully signed in!'
   end
