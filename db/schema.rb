6eeef125f10bf8723c639fe8cee1934db5327114
Icelle <icelleku@gmail.com>
2014-01-11 00:49:22 -0500

    generate model

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140111023925) do
+ActiveRecord::Schema.define(version: 20140111040924) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "member_informations", force: true do |t|
+    t.string   "key",        null: false
+    t.string   "value",      null: false
+    t.integer  "member_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "member_informations", ["member_id"], name: "index_member_informations_on_member_id", using: :btree
 
   create_table "members", force: true do |t|
     t.string   "first_name", null: false
