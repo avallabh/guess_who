4cb7371a04d1ad057999172d17f55ad9f659c915
Icelle <icelleku@gmail.com>
2014-01-10 23:28:18 -0500

    generate member model to make test pass

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140109203232) do
+ActiveRecord::Schema.define(version: 20140111023925) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "members", force: true do |t|
+    t.string   "first_name", null: false
+    t.string   "last_name",  null: false
+    t.string   "role",       null: false
+    t.string   "email",      null: false
+    t.string   "gender",     null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "github_uid",   null: false
