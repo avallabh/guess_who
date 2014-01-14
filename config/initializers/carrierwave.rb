a9485862c3fd9f5ed72edb7d531262173ce69268
Melissa Carbone <mcarbone1@gmail.com>
2014-01-14 16:32:40 -0500

    Fix AWS permissions

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,6 +1,6 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
-    provider: 'AWS'
+    provider: 'AWS',
     aws_access_key_id: ENV['AWSAccessKeyId'],
     aws_secret_access_key: ENV['AWSSecretKey']
   }
